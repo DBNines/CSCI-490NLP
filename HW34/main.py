41f56b71b0a93cba99320a06824d89e629568be3
Dylan <62079853+DBNines@users.noreply.github.com>
2026-02-08 02:07:37 +0000

    Add prob calc

--- HW34/main.py
+++ HW34/main.py
@@ -1,3 +1,5 @@
+import collections
+
 def getTextFromFile(filePath):
     with open(filePath, 'r') as file:
         return file.read()
@@ -10,9 +12,47 @@ def tokenize(inputText):
         tokens = text.split()
     return tokens
 
-#def biGramProb(tokens)
+def biGramProb(inputTokens):
+    biGrams = []
+    for i in range(len(inputTokens) - 1):
+        biGrams.append((inputTokens[i], inputTokens[i+1]))
+    uniGramCount = collections.Counter(inputTokens)
+    biGramCount = collections.Counter(biGrams)
+    #print(biGramCount)#print(uniGramCount)
+    probs = {}
+    for biGram, count in biGramCount.items():
+        probs[biGram] = count / uniGramCount[biGram[0]]
+    #print(probs)
+    return probs
+
+def biGramProbLaplace(inputTokens):
+    biGrams = []
+    for i in range(len(inputTokens) - 1):
+        biGrams.append((inputTokens[i], inputTokens[i+1]))
+    uniGramCount = collections.Counter(inputTokens)
+    biGramCount = collections.Counter(biGrams)
+    v = len(uniGramCount) # Number of unique words
+    probsLaplace = {}
+    for biGram, count in biGramCount.items():
+        probsLaplace[biGram] = (count + 1) / (uniGramCount[biGram[0]] + v)
+    #print(probs)
+    return probsLaplace
+
+def getTop5(inputProb):
+    sorted_probs = sorted(inputProb.items(), key=lambda x: x[1], reverse=True)
+    print("Top 5 Bigrams are:")
+    for i in range(0,5):
+        print(sorted_probs[i])
+
 
 def main():
     tokens = tokenize(getTextFromFile('input.txt'))
+    biGramProbs = biGramProb(tokens)
+    getTop5(biGramProbs)
+    #Laplace
+    biGramProbsLaplace = biGramProbLaplace(tokens)
+    print("################" + '\n' + "LAPLACE PROBS")
+    getTop5(biGramProbsLaplace)
+    print("################" + '\n' + "PERPLEXITY")
 
 main()
